912a854fdedbcd9dd51c05a769734f7d33849e7e
Tair <kuffo_tair@mail.ru>
2025-06-06 21:50:33 +0200

    Recovered all files from commit 7f379c69

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import AuthLayout from './layouts/AuthLayout';
+import HomePage from './pages/HomePage';
+import PlannerPage from './pages/PlannerPage';
+import GoogleCallback from './components/GoogleCallback';
+function AppContent() {
+  const location = useLocation();
+  const hideNavbar = ['/login', '/register'].includes(location.pathname);
+
+  return (
+    <>
+      {!hideNavbar && <Navbar />}
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/planner" element={<PlannerPage />} />
+        <Route path="/google/callback" element={<GoogleCallback />} />
+        
+      </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <UserProvider>
+      <Router>
+        <AppContent />
+      </Router>
+    </UserProvider>
+  );
+}
+
+export default App;
